f4e4b9c81e61e5d360ff1370fe79aa08b08d7174
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-12 12:44:57 +0300

    Update Id.py

--- MatrixMusic/plugins/play/Id.py
+++ MatrixMusic/plugins/play/Id.py
@@ -1,142 +1,95 @@
 import asyncio
+from asyncio import gather
 from pyrogram import Client, filters
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import os
+import time
+import aiohttp
+from pyrogram.types import CallbackQuery
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from MatrixMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from MatrixMusic import app
-import random
-from strings.filters import command
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-#BiLaL
+from telegraph import upload_file
+from asyncio import gather
+from pyrogram.errors import FloodWait
 
 
 iddof = []
-@app.on_message(
-    command(["قفل الايدي","تعطيل الايدي"])
-    & filters.group
-)
-async def iddlock(client, message):
-   get = await client.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in [5449190469]:
-      if message.chat.id in iddof:
-        return await message.reply_text("تم معطل من قبل \n√")
-      iddof.append(message.chat.id)
-      return await message.reply_text("تم تعطيل الايدي بنجاح √")
-   else:
-      return await message.reply_text("لازم تكون ادمن \n√")
+id = {}
 
-@app.on_message(
-    command(["فتح الايدي","تفعيل الايدي"])
-    & filters.group
-)
-async def iddopen(client, message):
-   get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in [5449190469]:
-      if not message.chat.id in iddof:
-        return await message.reply_text("الايدي مفعل من قبل √")
-      iddof.remove(message.chat.id)
-      return await message.reply_text("تم فتح الايدي بنجاح √")
-   else:
-      return await message.reply_text("لازم تكون ادمن \n√")
-
-
-
-
-@app.on_message(
-    command(["ايدي","id","ا"])
-    & filters.group
-)
-async def iddd(client, message):
-    if message.chat.id in iddof:
-      return
-    usr = await client.get_chat(message.from_user.id)
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"""⌯ 𝐍𝐚𝐦𝐞 :{message.from_user.mention}\n⌯ 𝐔𝐬𝐞𝐫 :@{message.from_user.username}\n⌯ 𝐈𝐝 :`{message.from_user.id}`\n⌯ 𝐁𝐢𝐨 :{usr.bio}\n⌯ 𝐂𝐡𝐚𝐭 : {message.chat.title}\n⌯ 𝐈𝐝 𝐜𝐡𝐚𝐭:`{message.chat.id}`""", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        ),
-    )
-
-
-
-iddof = []
-@app.on_message(
-    command(["قفل جمالي","تعطيل جمالي"])
-    & filters.group
-)
-async def lllock(client, message):
-   get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in [5449190469]:
-      if message.chat.id in iddof:
-        return await message.reply_text("جمالي معطل من قبل√")
-      iddof.append(message.chat.id)
-      return await message.reply_text(" تم تعطيل جمالي بنجاح√")
-   else:
-      return await message.reply_text("لازم تكون ادمن\n√")
-
-@app.on_message(
-    command(["فتح جمالي","تفعيل جمالي"])
-    & filters.group
-)
-async def idljjopen(client, message):
-   get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in [5449190469]:
-      if not message.chat.id in iddof:
-        return await message.reply_text("جمالي مفعل من قبل√")
-      iddof.remove(message.chat.id)
-      return await message.reply_text("تم فتح جمالي بنجاح √")
-   else:
-      return await message.reply_text("هذا الامر للأدمن فقط")
-
-
-
-@app.on_message(filters.command(['تفعيل التعديل'], prefixes=""))
-async def iddlock(client, message):
+@app.on_message(filters.command(["تعطيل الايدي", "قفل الايدي"], "") & filters.group)
+async def iddlock(client: Client, message):
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if get.status in [5449190469]:
-        if message.chat.id in italy:
-            return await message.reply_text("تم تفعيل التعديل \n√")
-        italy.append(message.chat.id)
-        return await message.reply_text("تم تفعيل التعديل بنجاح \n√")
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id in iddof:
+            return await message.reply_text("♪ الامر معطل من قبل .")
+        iddof.append(message.chat.id)
+        return await message.reply_text("♪ تم تعطيل الايدي بنجاح .")
     else:
-        return await message.reply_text("يجب عليك أن تكون مشرف اولا \n√")
+        return await message.reply_text("♪ عذرا عزيزي هذا الامر للادمن الجروب فقط .")
 
-@app.on_message(filters.command(['تعطيل التعديل'], prefixes=""))
-async def iddopen(client, message):
-   get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in [5449190469]:
-      if not message.chat.id in italy:
-        return await message.reply_text("التعديل معطل من قبل \n√")
-      italy.remove(message.chat.id)
-      return await message.reply_text("تم فتح تعطيل بنجاح \n√")
-   else:
-      return await message.reply_text("لازم تكون ادمن اولا \n√")
+@app.on_message(filters.command(["فتح الايدي", "تفعيل الايدي"], "") & filters.group)
+async def iddopen(client: Client, message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id not in iddof:
+            return await message.reply_text("♪ الايدي مفعل من قبل .")
+        iddof.remove(message.chat.id)
+        return await message.reply_text("♪ تم تفعيل الايدي بنجاح .")
+    else:
+        return await message.reply_text("♪ عذرا عزيزي هذا الامر للادمن الجروب فقط .")
 
-
-
-@app.on_message(
-    command(["جمالي"])
-    & filters.group
-)
-async def idjjdd(client, message):
+@app.on_message(filters.command(["ايدي","ا"], ""))
+async def muid(client: Client, message):
     if message.chat.id in iddof:
-      return
-    usr = await client.get_chat(message.from_user.id)
-    name = usr.first_name
-    i = ["0","10", "15","20", "25","30","35", "40","45", "50","55", "60"," 66", "70","77", "80","85", "90","99", "100","1000" ]
-    ik = random.choice(i)
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"نسبه جمالك يصاحبي \n※ \n🐉: {ik} %😂❤️", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        ),
-    )
-       
+        return await message.reply_text("♪ تم تعطيل امر الايدي من قبل المشرفين .")
+    
+    user = await client.get_chat(message.from_user.id)
+    user_id = user.id
+    username = user.username
+    first_name = user.first_name
+    bio = user.bio
+    chat = message.chat.title
+    chat_id = message.chat.id
+   
+    photo = user.photo.big_file_id
+    if photo:
+        photo = await client.download_media(photo)
+    else:
+        photo = None
+    
+    if user.id not in id:
+        id[user.id] = []
+    
+    idd = len(id[user.id])
+    
+    caption = f"𝐍𝐚𝐦𝐞 : {first_name}\n𝐢𝐝 : {user_id}\n𝐔𝐬𝐞𝐫 : [@{username}]\n𝐁𝐢𝐨 : {bio}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd} 🖤", callback_data=f"heart{user_id}")]])
+    
+    await message.reply_photo(photo=photo, caption=caption, reply_markup=reply_markup)
+
+@app.on_callback_query(filters.regex("heart"))
+async def heart(client, query: CallbackQuery):
+    callback_data = query.data.strip()
+    callback_request = callback_data.replace("heart", "")
+    user_id = int(callback_request)
+    user = await client.get_chat(user_id)
+    
+    if user.id not in id:
+        id[user.id] = []
+    
+    if query.from_user.mention not in id[user.id]:
+        id[user.id].append(query.from_user.mention)
+    else:
+        id[user.id].remove(query.from_user.mention)
+    
+    idd = len(id[user.id])
+    
+    caption = f"𝐍𝐚𝐦𝐞 : {first_name}\n𝐢𝐝 : {user_id}\n𝐔𝐬𝐞𝐫 : [@{username}]\n𝐁𝐢𝐨 : {bioo}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd} 🖤", callback_data=f"heart{user_id}")]])
+    
+    await query.edit_message_text(caption, reply_markup=reply_markup)
